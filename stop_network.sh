7b6188d07a5bf723f9809d348a545a3dcc4a6ca2
blockpower <53705927+blockpower@users.noreply.github.com>
2019-09-02 21:39:36 +0200

    Update stop_network.sh

--- stop_network.sh
+++ stop_network.sh
@@ -8,6 +8,11 @@
 netstat -putan | grep 3000 | cut -d "/" -f 1 | awk '{ system("kill -9 "$7 ) }'
 sleep 5
 
+# Stop Angular app process
+netstat -putan | grep 4200 | cut -d "/" -f 1 | awk '{ system("kill -9 "$7 ) }'
+sleep 5
+
+
 # Stop Hyperledger Fabic Network
 cd ~/fabric-dev-servers/
 ./stopFabric.sh
